d453c4a611fff868c4f0ca4f2bed12d823c45eba
Jackie <41758618+jgzz28@users.noreply.github.com>
2020-06-19 15:29:24 -0500

    Create tenminutewalk.js
    added description and code

---
+++ tenminutewalk.js
@@ -0,0 +1,61 @@
+function isValidWalk(walk) {
+  //insert brilliant code here
+  
+  //Safe test
+  if((walk.length < 0) || (walk.length > 10)){
+    return false;
+  }
+  
+  //Safe test
+  else if(walk.length < 10){
+    return false;
+  }
+  
+  //Exact number neither late or early
+  else if(walk.length = 10){
+  
+    var x = 0;
+    var y = 0;
+    
+     //If it does not return you to your starting point
+    for(i = 0; i < walk.length; i++)
+    {
+      switch(walk[i])
+      {
+        case 'n': x++; break;
+        case 's': x--; break;
+        case 'w': y--; break;
+        case 'e': y++; break;
+      }
+      
+      console.log(x);
+      console.log(y);
+    }
+    
+    //If back at starting point, return true
+    if((x == 0) && (y == 0))
+    {
+      return true;
+    }
+    
+    else{
+      return false;
+    }
+  }
+}
+
+/* You live in the city of Cartesia where all roads are laid out in a perfect grid. 
+You arrived ten minutes too early to an appointment, so you decided to take the 
+opportunity to go for a short walk. The city provides its citizens with a 
+Walk Generating App on their phones -- everytime you press the button it 
+sends you an array of one-letter strings representing directions to walk 
+(eg. ['n', 's', 'w', 'e']). You always walk only a single block in a direction 
+and you know it takes you one minute to traverse one city block, so create a function
+that will return true if the walk the app gives you will take you exactly ten minutes 
+(you don't want to be early or late!) and will, of course, return you to your starting point.
+Return false otherwise.
+
+Note: you will always receive a valid array containing a random assortment 
+of direction letters ('n', 's', 'e', or 'w' only). It will never give you an empty array 
+(that's not a walk, that's standing still!).
+*/
